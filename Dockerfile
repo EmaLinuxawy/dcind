d4431499dc5923d59e0bed66cd9ee3ae71dba8b1
Emalinuxawy <emalinuxawy@gmail.com>
2023-07-20 15:14:12 -0600

    support latest docker features

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,32 @@
 # Inspired by https://github.com/mumoshu/dcind
-FROM alpine:3.10
+FROM alpine:3.18.2
 LABEL maintainer="Dmitry Matrosov <amidos@amidos.me>"
+LABEL maintainer="Ibrahim Soliman <Emalinuxawy@gmail.com>"
 
-ENV DOCKER_VERSION=18.09.8 \
-    DOCKER_COMPOSE_VERSION=1.24.1
+ENV DOCKER_VERSION=23.0.6
 
 # Install Docker and Docker Compose
-RUN apk --no-cache add bash curl util-linux device-mapper py-pip python-dev libffi-dev openssl-dev gcc libc-dev make iptables && \
-    curl https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz | tar zx && \
+RUN apk add --no-cache \
+    bash \
+    build-base \
+    ca-certificates \
+    curl \
+    gcc \
+    git \
+    libc-dev \
+    libffi-dev \
+    libgcc \
+    make \
+    musl-dev \
+    openssl \
+    openssl-dev \
+    zlib-dev \
+    util-linux \
+    device-mapper
+
+RUN curl https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz | tar zx && \
     mv /docker/* /bin/ && \
     chmod +x /bin/docker* && \
-    pip install docker-compose==${DOCKER_COMPOSE_VERSION} && \
     rm -rf /root/.cache
 
 # Include functions to start/stop docker daemon
--- docker-lib.sh
+++ docker-lib.sh
@@ -3,10 +3,13 @@
 
 LOG_FILE=${LOG_FILE:-/tmp/docker.log}
 SKIP_PRIVILEGED=${SKIP_PRIVILEGED:-false}
-STARTUP_TIMEOUT=${STARTUP_TIMEOUT:-20}
-DOCKER_DATA_ROOT=${DOCKER_DATA_ROOT:-/scratch/docker}
+STARTUP_TIMEOUT=${STARTUP_TIMEOUT:-120}
 
 sanitize_cgroups() {
+  if [ -e /sys/fs/cgroup/cgroup.controllers ]; then
+    return
+  fi
+
   mkdir -p /sys/fs/cgroup
   mountpoint -q /sys/fs/cgroup || \
     mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
@@ -45,20 +48,13 @@ sanitize_cgroups() {
     fi
   done
 
-  if ! test -e /sys/fs/cgroup/systemd ; then
+  if [ ! -e /sys/fs/cgroup/systemd ] && [ $(cat /proc/self/cgroup | grep '^1:name=openrc:' | wc -l) -eq 0 ]; then
     mkdir /sys/fs/cgroup/systemd
     mount -t cgroup -o none,name=systemd none /sys/fs/cgroup/systemd
   fi
 }
 
 start_docker() {
-  echo "Starting Docker..."
-
-  if [ -f /tmp/docker.pid ]; then
-    echo "Docker is already running"
-    return
-  fi
-
   mkdir -p /var/log
   mkdir -p /var/run
 
@@ -75,19 +71,18 @@ start_docker() {
   local server_args="--mtu ${mtu}"
   local registry=""
 
-  for registry in $1; do
+#  server_args="${server_args} --max-concurrent-downloads=$1 --max-concurrent-uploads=$2"
+
+  for registry in $3; do
     server_args="${server_args} --insecure-registry ${registry}"
   done
 
-  if [ -n "$2" ]; then
-    server_args="${server_args} --registry-mirror $2"
+  if [ -n "$4" ]; then
+    server_args="${server_args} --registry-mirror $4"
   fi
 
-  export server_args LOG_FILE DOCKER_DATA_ROOT
-  trap stop_docker EXIT
-
   try_start() {
-    dockerd --data-root $DOCKER_DATA_ROOT ${server_args} >$LOG_FILE 2>&1 &
+    dockerd --data-root /scratch/docker ${server_args} >$LOG_FILE 2>&1 &
     echo $! > /tmp/docker.pid
 
     sleep 1
@@ -101,30 +96,123 @@ start_docker() {
     done
   }
 
-  if [ "$(command -v declare)" ]; then
-    declare -fx try_start
+  export server_args LOG_FILE
+  declare -fx try_start
+  trap stop_docker EXIT
 
-    if ! timeout ${STARTUP_TIMEOUT} bash -ce 'while true; do try_start && break; done'; then
-      echo Docker failed to start within ${STARTUP_TIMEOUT} seconds.
-      return 1
-    fi
-  else
-    try_start
+  if ! timeout ${STARTUP_TIMEOUT} bash -ce 'while true; do try_start && break; done'; then
+    [ -f "$LOG_FILE" ] && cat "${LOG_FILE}"
+    echo Docker failed to start within ${STARTUP_TIMEOUT} seconds.
+    return 1
   fi
 }
 
 stop_docker() {
-  echo "Stopping Docker..."
-
-  if [ ! -f /tmp/docker.pid ]; then
-    return 0
-  fi
-
   local pid=$(cat /tmp/docker.pid)
   if [ -z "$pid" ]; then
     return 0
   fi
 
   kill -TERM $pid
-  rm /tmp/docker.pid
+}
+
+log_in() {
+  local username="$1"
+  local password="$2"
+  local registry="$3"
+
+  if [ -n "${username}" ] && [ -n "${password}" ]; then
+    echo "${password}" | docker login -u "${username}" --password-stdin ${registry}
+  else
+    mkdir -p ~/.docker
+    touch ~/.docker/config.json
+    # This ensures the resulting JSON object remains syntactically valid
+    echo "$(cat ~/.docker/config.json){\"credsStore\":\"ecr-login\"}" | jq -s add > ~/.docker/config.json
+  fi
+}
+
+private_registry() {
+  local repository="${1}"
+
+  local registry="$(extract_registry "${repository}")"
+  if echo "${registry}" | grep -q -x '.*[.:].*' ; then
+    return 0
+  fi
+
+  return 1
+}
+
+extract_registry() {
+  local repository="${1}"
+
+  echo "${repository}" | cut -d/ -f1
+}
+
+extract_repository() {
+  local long_repository="${1}"
+
+  echo "${long_repository}" | cut -d/ -f2-
+}
+
+image_from_tag() {
+  docker images --no-trunc "$1" | awk "{if (\$2 == \"$2\") print \$3}"
+}
+
+image_from_digest() {
+  docker images --no-trunc --digests "$1" | awk "{if (\$3 == \"$2\") print \$4}"
+}
+
+certs_to_file() {
+  local raw_ca_certs="${1}"
+  local cert_count="$(echo $raw_ca_certs | jq -r '. | length')"
+
+  for i in $(seq 0 $(expr "$cert_count" - 1));
+  do
+    local cert_dir="/etc/docker/certs.d/$(echo $raw_ca_certs | jq -r .[$i].domain)"
+    mkdir -p "$cert_dir"
+    echo $raw_ca_certs | jq -r .[$i].cert >> "${cert_dir}/ca.crt"
+  done
+}
+
+set_client_certs() {
+  local raw_client_certs="${1}"
+  local cert_count="$(echo $raw_client_certs | jq -r '. | length')"
+
+  for i in $(seq 0 $(expr "$cert_count" - 1));
+  do
+    local cert_dir="/etc/docker/certs.d/$(echo $raw_client_certs | jq -r .[$i].domain)"
+    [ -d "$cert_dir" ] || mkdir -p "$cert_dir"
+    echo $raw_client_certs | jq -r .[$i].cert >> "${cert_dir}/client.cert"
+    echo $raw_client_certs | jq -r .[$i].key >> "${cert_dir}/client.key"
+  done
+}
+
+docker_pull() {
+  GREEN='\033[0;32m'
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+
+  pull_attempt=1
+  max_attempts=3
+  while [ "$pull_attempt" -le "$max_attempts" ]; do
+    printf "Pulling ${GREEN}%s${NC}" "$1"
+
+    if [ "$pull_attempt" != "1" ]; then
+      printf " (attempt %s of %s)" "$pull_attempt" "$max_attempts"
+    fi
+
+    printf "...\n"
+
+    if docker pull "$1"; then
+      printf "\nSuccessfully pulled ${GREEN}%s${NC}.\n\n" "$1"
+      return 0
+    fi
+
+    echo
+
+    pull_attempt=$(expr "$pull_attempt" + 1)
+  done
+
+  printf "\n${RED}Failed to pull image %s.${NC}" "$1"
+  return 1
 }
